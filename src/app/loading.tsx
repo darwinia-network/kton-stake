f5ddcea5b1d500c386fa9acd75250e20b145e0b9
snoopy1412 <snoopy1412.su@gamil.com>
2024-03-05 20:38:10 +0800

    Update height of loading skeleton

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 const Loading = () => {
   return (
     <div className="flex h-full w-full items-center justify-center px-10 ">
-      <Skeleton className="container flex h-[400px] flex-col items-center justify-center gap-5 rounded-[1.25rem] bg-[#242A2E] p-5 sm:w-[25rem]"></Skeleton>
+      <Skeleton className="container flex h-[26.5rem] flex-col items-center justify-center gap-5 rounded-[1.25rem] bg-[#242A2E] p-5 sm:w-[25rem]"></Skeleton>
     </div>
   );
 };
