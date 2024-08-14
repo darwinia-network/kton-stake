02d71cca59e9a5c8e3a4db977aaa0a0ac3cde8de
fisher <denny.wang@itering.io>
2024-08-14 16:51:34 +0800

    Update src/config/v2migrate.ts

--- src/config/v2migrate.ts
+++ src/config/v2migrate.ts
@@ -1,4 +1,4 @@
 import { ChainId } from "@/types/chains";
 
 // TODO: add chain id into array when rolling out V2 in the chain
-export const v2ChainId: ChainId[] = [ChainId.KOI, ChianId.CRAB]
+export const v2ChainId: ChainId[] = [ChainId.KOI, ChainId.CRAB]
